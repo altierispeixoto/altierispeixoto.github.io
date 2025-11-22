5fb0ae7df20a91006d87506941ee406396af6f60
Altieris Peixoto <altieris.marcelino@gmail.com>
2025-11-23 11:34:55 +1300

    chore: hidding draft posts

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/alt.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Altieris Peixoto",
+	bio: "Lead Data Scientist @ Auror",
 	links: [
 		{
 			name: "LinkedIn",
