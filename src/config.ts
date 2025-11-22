ace36094a3b83de767589c52a8233dc9a37ab95c
alt <altierispeixoto@users.noreply.github.com>
2025-11-23 12:30:08 +1300

    Fix/config (#8)

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: false,
-		src: "assets/images/alt.jpeg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/111024784_p0_master1200.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			text: "空色天絵 / NEO TOKYO NOIR 01", // Credit text to be displayed
+			url: "https://www.pixiv.net/en/artworks/111024784", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
